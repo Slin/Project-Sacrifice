9e13aeddc5abdea0c4a7b1febb22b75b6a61a7b7
Nils Daumann <nils_daumann@slindev.com>
2015-11-09 17:51:31 +0100

    Box2D find blubb

--- cmake_modules/FindBox2D.cmake
+++ cmake_modules/FindBox2D.cmake
@@ -1,30 +1,63 @@
-# source: http://breathe.git.sourceforge.net
-#
-# Locate Box2D library
-# This module defines
-# BOX2D_LIBRARY, the name of the library to link against
-# BOX2D_FOUND, if false, do not try to link to Box2D
-# BOX2D_INCLUDE_DIR, where to find Box2D headers
-#
-# Created by Sven-Hendrik Haase. Based on the FindZLIB.cmake module.
+# Locate Box2D
+# This module defines XXX_FOUND, XXX_INCLUDE_DIRS and XXX_LIBRARIES standard variables
 
-IF(BOX2D_INCLUDE_DIR)
-  # Already in cache, be silent
-  SET(BOX2D_FIND_QUIETLY TRUE)
-ENDIF(BOX2D_INCLUDE_DIR)
+FIND_PATH(BOX2D_INCLUDE_DIR Box2D.h
+	HINTS
+	$ENV{BOX2D_DIR}
+	$ENV{BOX2D_PATH}
+	${ADDITIONAL_SEARCH_PATHS}
+	PATH_SUFFIXES include Include
+	PATHS
+		~/Library/Frameworks
+		/Library/Frameworks
+		/usr/local
+		/usr
+		/sw # Fink
+		/opt/local # DarwinPorts
+		/opt/csw # Blastwave
+		/opt
+)
 
-FIND_PATH(BOX2D_INCLUDE_DIR Box2D/Box2D.h)
+FIND_LIBRARY(BOX2D_LIBRARY
+	NAMES box2d
+	HINTS
+	$ENV{BOX2D_DIR}
+	$ENV{BOX2D_PATH}
+	${ADDITIONAL_SEARCH_PATHS}
+	PATH_SUFFIXES lib64 lib lib/release Library
+	PATHS
+		~/Library/Frameworks
+		/Library/Frameworks
+		/usr/local
+		/usr
+		/sw
+		/opt/local
+		/opt/csw
+		/opt
+)
 
-SET(BOX2D_NAMES box2d Box2d BOX2D Box2D)
-FIND_LIBRARY(BOX2D_LIBRARY NAMES ${BOX2D_NAMES})
-MARK_AS_ADVANCED(BOX2D_LIBRARY BOX2D_INCLUDE_DIR)
+FIND_LIBRARY(BOX2D_LIBRARY_DEBUG 
+	NAMES box2dd box2d_d
+	HINTS
+	$ENV{BOX2D_DIR}
+	$ENV{BOX2D_PATH}
+	${ADDITIONAL_SEARCH_PATHS}
+	PATH_SUFFIXES lib64 lib lib/debug Library
+	PATHS
+		~/Library/Frameworks
+		/Library/Frameworks
+		/usr/local
+		/usr
+		/sw
+		/opt/local
+		/opt/csw
+		/opt
+)
 
-# Per-recommendation
-SET(BOX2D_INCLUDE_DIRS "${BOX2D_INCLUDE_DIR}")
-SET(BOX2D_LIBRARIES    "${BOX2D_LIBRARY}")
 
-# handle the QUIETLY and REQUIRED arguments and set BOX2D_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set CURL_FOUND to TRUE if 
 # all listed variables are TRUE
-
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Box2D DEFAULT_MSG BOX2D_LIBRARY BOX2D_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(BOX2D DEFAULT_MSG BOX2D_LIBRARY BOX2D_INCLUDE_DIR)
+INCLUDE(FindPackageTargetLibraries)
+FIND_PACKAGE_SET_STD_INCLUDE_AND_LIBS(BOX2D)
